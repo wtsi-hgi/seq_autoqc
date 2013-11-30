8dc3b821acae0efceddac882a18ab77b06991cfb
Joshua C. Randall <jcrandall@alum.mit.edu>
2013-11-30 16:30:25 +0000

    Improves validation of options

--- bamcheck_augment_summary.R
+++ bamcheck_augment_summary.R
@@ -23,35 +23,39 @@
 
 library(optparse)
 
+indel_baseline_methods <- c("runmed", "runmed_quadratic", "quadratic", "runmin_quadratic")
+base_content_baseline_methods <- c("mean", "median", "runmed")
+
 option_list <- list(
   make_option(c("--indel-baseline-method"), action = "store", 
   	      type = "character", default = "runmed", metavar = "method",
-	      help = paste(sep=" ",
-	                   "Baseline method for indel",
-			   "(one of 'runmed','runmed_quadratic', 'quadratic',", 
-			   "or 'runmin_quadratic')")),
+	      help = paste(sep = "",
+	                   "Baseline method for indels (one of",
+			   paste0(collapse = ", ", sprintf("'%s'", indel_baseline_methods)),
+			   ")")),
   make_option(c("--indel-runmed-k"), action = "store",
-  	      type = "integer", default = 25, metavar = "k",
+  	      type = "numeric", default = 25, metavar = "k",
 	      help = paste(sep=" ",
 	                   "Number of read cycles to include in running median for",
 	             	   "indel peak detection")),
-  make_option(c("--base-content-baseline-method"), action = "store",
-  	      type = "character", default = "mean", metavar = "method",
-	      help = paste(sep=" ",
-	                   "Baseline method for base content deviation",
-	                   "(one of 'mean', 'median', or 'runmed')")),
   make_option(c("--base-content-runmed-k"), action = "store", 
-              type = "integer", default = 25, metavar = "k",
+              type = "numeric", default = 25, metavar = "k",
 	      help = paste(sep=" ",
 	                   "Number of read cycles to include in running median",
 			   "for base content deviation")),
+  make_option(c("--base-content-baseline-method"), action = "store",
+  	      type = "character", default = "mean", metavar = "method",
+	      help = paste(sep="",
+	                   "Baseline method for base content deviation (one of",
+			   paste0(collapse = ", ", sprintf("'%s'", base_content_baseline_methods)),
+			   ")")),
   make_option(c("--quality-dropoff-runmed-k"), action = "store", 
-              type = "integer", default = 25, metavar = "k",
+              type = "numeric", default = 25, metavar = "k",
      	      help = paste(sep=" ",
 	                   "Number of read cycles to include in running median",
 			   "for quality dropoff")),
   make_option(c("--quality-dropoff-ignore-edge-cycles"), action = "store", 
-              type = "integer", default = 3, metavar = "cycle-count",
+              type = "numeric", default = 3, metavar = "cycle-count",
 	      help = paste(sep=" ",
 	                   "Number of cycles at beginning and end of read to",
 			   "exclude from quality dropoff")),
@@ -70,29 +74,71 @@ op <- OptionParser(option_list = option_list, usage = usage)
 options_args <- parse_args(op, positional_arguments = TRUE)
 
 if(length(options_args$args) != 2) {
+  error_msg <- "Unknown error"
   possible_opts = options_args$args[substr(options_args$args, 1, 1) == "-"]
   if (length(possible_opts) > 0) {
-    if (length(possible_opts) == 1) { 
-      cat(sprintf("Unrecognized option: %s\n", possible_opts[1]))
-    } else {
-      cat(sep = "\n", "Unrecognized options:", paste0("  ", possible_opts))
-    }
+    error_msg <- paste0(sep = "\n", 
+                        "Unrecognized option: ", 
+	  	        possible_opts)
   } else {
-    cat(sprintf("Must specify exactly two arguments (had %d)\n", length(options_args$args)))
-    cat("Arguments were:\n ", paste0(collapse = "\n  ", options_args$args), "\n")
+    error_msg <- paste(sep = "\n",
+                     sprintf("Must specify exactly two arguments (had %d)", length(options_args$args)),
+    		     "Arguments were:", 
+		     paste0("  ", options_args$args))
   }
   print_help(op)
-  stop()
+  stop(call. = FALSE, error_msg)
 }
 
+stop_if_invalid_opt_nonmissing <- function(opts, optkey) {
+  if (is.na(opts[[optkey]])) {
+    stop(call. = FALSE, sprintf("%s is invalid or missing", paste(sep = "", "--", optkey)))
+  }
+}
+
+stop_if_invalid_opt_posnum <- function(opts, optkey) {
+  stop_if_invalid_opt_nonmissing(opts, optkey)
+  if (! is.numeric(opts[[optkey]])) {
+    stop(call. = FALSE, sprintf("%s=%s is not numeric", paste(sep = "", "--", optkey), opts[[optkey]]))
+  }
+  if (! opts[[optkey]] > 0) {
+    stop(call. = FALSE, sprintf("%s=%s is not > 0", paste(sep = "", "--", optkey), opts[[optkey]]))
+  }
+}
+
+stop_if_invalid_opt_posint <- function(opts, optkey) {
+  stop_if_invalid_opt_posnum(opts, optkey)
+  if (! as.integer(opts[[optkey]]) == opts[[optkey]]) {
+    stop(call. = FALSE, sprintf("%s=%s is not an integer", paste(sep = "", "--", optkey), opts[[optkey]]))
+  }
+}
+
+stop_if_invalid_opt_method <- function(opts, optkey, valid_methods) {
+  stop_if_invalid_opt_nonmissing(opts, optkey)
+  if (! opts[[optkey]] %in% valid_methods) {
+    stop(call. = FALSE, sprintf("%s is not a valid %s (must be one of %s)", opts[[optkey]], paste(sep = "", "--", optkey), paste0(collapse = ", ", valid_methods)))
+  }
+}
+
+# validate options
+stop_if_invalid_opt_posint(options_args$options, "indel-runmed-k")
+stop_if_invalid_opt_method(options_args$options, "indel-baseline-method", indel_baseline_methods)
+stop_if_invalid_opt_posint(options_args$options, "base-content-runmed-k")
+stop_if_invalid_opt_method(options_args$options, "base-content-baseline-method", base_content_baseline_methods)
+stop_if_invalid_opt_posint(options_args$options, "quality-dropoff-runmed-k")
+stop_if_invalid_opt_posint(options_args$options, "quality-dropoff-ignore-edge-cycles")
+
+# set input and output files
 bamcheck_input <- options_args$args[1]
 bamcheck_output <- options_args$args[2]
 
-
+# require bamcheckr package
 library(bamcheckr)
 
+# read from bamcheck file
 bamcheck <- read_bamcheck(bamcheck_input)
 
+# augment bamcheck summary numbers 
 bamcheck$data$SN <- rbind(bamcheck$data$SN, 
 		    	  indel_peaks(bamcheck = bamcheck, 
 			              baseline_method = options_args$options[["indel-baseline-method"]],
@@ -108,9 +154,10 @@ bamcheck$data$SN <- rbind(bamcheck$data$SN,
 						 runmed_k = options_args$options[["base-content-runmed-k"]],
 						 outplotbase = options_args$options[["plot-base-path"]]))
 
-# Add provenance notes to header
+# add provenance notes to header
 bamcheck$comments$HEADER <- append(bamcheck$comments$HEADER, "# It was modified by bamcheck_augment_summary.R")
 bamcheck$comments$HEADER <- append(bamcheck$comments$HEADER, paste("# R command line was: ", paste0(commandArgs(), collapse=" ")))
 
-# Write augmented bamcheck file
+# write augmented bamcheck file
 write_bamcheck(bamcheck = bamcheck, bamcheck_file = bamcheck_output)
+
